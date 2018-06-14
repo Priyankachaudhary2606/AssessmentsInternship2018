cc0890390120da79aa644ea859893928a84b6356
sagun01 <40224371+sagun01@users.noreply.github.com>
2018-06-14 19:01:24 +0530

    Author name: Sagun Saluja ADD FILE: Controller.java Description: controller file

---
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/controller/Controller.java
@@ -0,0 +1,36 @@
+package com.nskfdc.scgj.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nskfdc.scgj.service.chefservice;
+
+@RestController
+public class Controller {
+	@Autowired
+	private chefservice obj1;
+	
+	@RequestMapping("/getmomos")
+	public String getmomosmethod()
+	{
+	 return "bigburger";
+}
+
+	
+	@RequestMapping("/getspicymomos")
+	public String getspicymomosmethod(@RequestParam("value") String any)
+	{
+	 return obj1.momos(any);
+}
+	
+
+	@RequestMapping("/getfriedmomos")
+	public String getfriedmomosmethod(@RequestParam("value") String any, @RequestParam("id") String box)
+	{
+	 return "have momos" +any +box;
+}
+}
+	
+	
