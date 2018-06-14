091b4e98c490acc9f055c94869aeb500fc2dfa7c
ayushsnb <40201444+ayushsnb@users.noreply.github.com>
2018-06-14 19:13:11 +0530

    Author:Ayush Jain File name: MoneyService.java Description: Implementation of Service

---
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/service/MoneyService.java
@@ -0,0 +1,13 @@
+package com.nskfdc.scgj.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MoneyService {
+public String getmymoney(String a, String b)
+{
+	String c=a+b;
+	
+	return c;
+}
+}
