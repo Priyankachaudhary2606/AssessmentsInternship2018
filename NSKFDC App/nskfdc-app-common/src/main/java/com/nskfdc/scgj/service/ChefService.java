4e8baf30be2f3cb357f51443de8061e536e2829f
mahekgupta17 <40226383+mahekgupta@users.noreply.github.com>
2018-06-14 18:47:11 +0530

    Author:Mahek Gupta files added:Controller Waiter1controller.java and ChefService.java added. Description: practice

--- NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/service/ChefService.java
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/service/ChefService.java
@@ -2,11 +2,21 @@ package com.nskfdc.scgj.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class ChefService {
-	public static String getBurgerMethod(String addcomment) 
-	{
-		return "Big Burger with " +addcomment;
-	}
+
+	@Service
+	
+	public class chefservice{
+		
+		public static String makeBurger(String Comment1, String Comment2 )
+		{
+			return"Big Burgers"+Comment1+Comment2;
+		}
+public static String makeMomos(String Comment1)
+{
+	return "Yummy Momos"+Comment1;
+	
+}
 
 }
+
+
