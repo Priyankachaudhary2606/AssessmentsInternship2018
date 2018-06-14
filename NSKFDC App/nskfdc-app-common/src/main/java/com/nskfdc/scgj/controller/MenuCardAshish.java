715e29fcf1ca7adf41a2437d2a91a389d2967cf9
AshishVerma4 <40226678+AshishVerma4@users.noreply.github.com>
2018-06-14 17:49:44 +0530

    Author: Ashish Verma Description: Hands on practise on controller for momos shop Files Added:MenuCardAshish.java

---
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/controller/MenuCardAshish.java
@@ -0,0 +1,36 @@
+package com.nskfdc.scgj.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nskfdc.scgj.service.ChefService;
+@RestController
+public class MenuCardAshish {
+	@Autowired
+	public ChefService chef;
+	
+	//without any request parametre
+	@RequestMapping("/giveMomos")
+	public String giveMomos(){
+		return ChefService.Momos();
+	}
+	
+	//with one request parametre
+     @RequestMapping("/giveFriedMomos")
+     public String giveFriedMomos(@RequestParam("comment") String Additionalcomment)
+     {
+    	 return ChefService.FriedMomos(Additionalcomment);
+    	 
+     }
+     
+     //with two request parametres
+     @RequestMapping("/givePaneerMomos")
+     public String givePaneerMomos(@RequestParam("comment1") String Additionalcomment1,
+    		 @RequestParam("comment2") String Additionalcomment2){
+      return ChefService.PaneerMomos(Additionalcomment1,Additionalcomment2);   	 
+    	 
+     }
+ 
+}
