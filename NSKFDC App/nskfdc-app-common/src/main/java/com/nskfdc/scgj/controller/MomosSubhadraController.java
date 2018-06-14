5274f3256ff062d620c0db64a74feb6229612e36
Subhadra01 <40224353+Subhadra01@users.noreply.github.com>
2018-06-14 18:47:26 +0530

    author:Subhadra file added:MomosSubhadraController.java and RestController.java Description:for practicing controller

---
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/controller/MomosSubhadraController.java
@@ -0,0 +1,29 @@
+package com.nskfdc.scgj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MomosSubhadraController {
+	
+	@RequestMapping("/getmomos")
+	public String getMomosMethod()
+	{
+		return "spicy momos";
+	}
+	
+	@RequestMapping("/getspicymomos")
+	public String getSpicyMomos(@RequestParam("topping") String suggestedTopping )
+	{
+		return "Subhadra momos"+suggestedTopping;
+		
+	}
+	@RequestMapping("/getfriedspicymomos")
+	public String getFriedSpicyMomos(@RequestParam("topping") String suggestedTopping, @RequestParam("type") String fried)
+	{
+		return "Subhadra momos"+suggestedTopping+fried;
+		
+	}
+
+}
---
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/controller/RestController.java
@@ -0,0 +1,5 @@
+package com.nskfdc.scgj.controller;
+
+public @interface RestController {
+
+}
