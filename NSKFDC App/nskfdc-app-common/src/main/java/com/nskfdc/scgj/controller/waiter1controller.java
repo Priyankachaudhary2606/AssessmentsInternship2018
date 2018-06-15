ef31753855d531b97a560475887499443a879a8c
mahekgupta17 <40226383+mahekgupta@users.noreply.github.com>
2018-06-15 14:42:00 +0530

    Author:Mahek Gupta file added:controller waiter1controller.java and chefservice.class description: practice

--- NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/controller/waiter1controller.java
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/controller/waiter1controller.java
@@ -5,29 +5,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nskfdc.scgj.service.ChefController;
+import com.nskfdc.scgj.service.chefservice;
 
+
+	
 @RestController
-public class waiter1controller 
-{
-	
-	@Autowired
-	public ChefController chef;
-	
-	@RequestMapping("/getSpoons")
-	public String getSpoons()
-	{
-    return "Have the spoons";
+ public class Waiter1controller{
+		@Autowired
+		public chefservice chef;
+		@RequestMapping("/getSpoons")
+		
+		public String getSpoons(){
+			return"have Spoons";
 		}
-@RequestMapping("/getBurgers")
-public String makeBurgers(@RequestParam("comment")String addcomment){
-	return ChefController.makeBurgers(addcomment);
-}
-@RequestMapping("/getMomos")
-public String makeMomos(@RequestParam("comment 1")String addcomment1,@RequestParam("comment 2")String addcomment2){
-	{
-		return ChefController.makeMomos(addcomment1,addcomment2);
-	}
-}
+		@RequestMapping("/getBurger")
+		
+		public String String(@RequestParam("Comment1")String addComment1,@RequestParam("Comment2")String addComment2 ){
+			return chefservice.makeBurger(addComment1,addComment2);
+			
+		}
+			
+		@RequestMapping("/getMomos")
+		public String String2(@RequestParam("Comment1")String addComment1){
+			return chefservice.makeMomos( addComment1);
+			
+		}
+	
+		
 
+		
+		
+	
 }
