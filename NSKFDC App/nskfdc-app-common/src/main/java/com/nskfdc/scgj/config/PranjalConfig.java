3ad367f3efb5017bcbbf00ce578fc8da0df4fcb3
Pranjal Kumar Mani <38162040+pranjalmani123@users.noreply.github.com>
2018-06-21 00:06:00 +0530

    Author:Pranjal Kumar Mani File:PranjalConfig.java Description:Backend Assessment

---
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/config/PranjalConfig.java
@@ -0,0 +1,19 @@
+package com.nskfdc.scgj.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="pranjalQuery", locations = "classpath:sql/pranjalquery.yml")
+public class PranjalConfig {
+	private String showCustomerDetails;
+ 	public String getShowCustomerDetails()
+ 	{
+ 		return showCustomerDetails;
+ 	}
+	public void setShowCustomerDetails(String showCustomerDetails)
+	{
+		this.showCustomerDetails = showCustomerDetails;
+
+}
+}
