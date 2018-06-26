015ecf0b8ef66595bb0649fbaa0e65ab9ae99c28
prateek-kapoor323 <prateek@smaltandberyl.com>
2018-06-27 00:27:34 +0530

    Author          : Prateek Kapoor File Modified   : PranjalController.java Description     : Fixed imports for annotations

--- NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/controller/PranjalController.java
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/controller/PranjalController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.nskfdc.scgj.dto.PranjalDto;
 import com.nskfdc.scgj.service.PranjalService;
@@ -12,7 +13,7 @@ import com.nskfdc.scgj.service.PranjalService;
 public class PranjalController {
 	@Autowired
 	private PranjalService pranjalService;
-	@RequestMapping("/getPranjalAirtel")
+	@RequestMapping("/getPranjalAirteDetails")
 	public Collection<PranjalDto> getCustomerDetails(){
 		try {
 			return pranjalService.getCustomerDetails();
